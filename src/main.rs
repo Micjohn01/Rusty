9261a47dc63abac2c4716e2b4535e96675b4fa2a
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-01-16 19:11:56 +0100

    getting my hand a bit dirty

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod primitive_datatypes;
+
+
+fn main() {
+    // println!("Hello, world!");
+
+    primitive_datatypes::primitive();
+    primitive_datatypes::floating();
+}
---
+++ src/primitive_datatypes.rs
@@ -0,0 +1,23 @@
+// Primitifve Data Types
+// 1. Integer
+// 2. Float
+// 3. Boolean
+// 4. Char
+
+pub fn primitive() {
+    let x: i32 = 42;
+    let y: u64 = 200;
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+}
+
+pub fn floating () {
+    let x: f32 = 45.3;
+    let y: f64 = 50.55;
+    println!("Floating number:{}", x);
+    println!("Double number:{}", y);
+
+    let is_snowing: bool = true;
+    println!("Is snowing: {}", is_snowing);
+
+}
