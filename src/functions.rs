98efd77d62c045c871dc8c55917ad86884460507
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-01-19 17:17:44 +0100

    expression over statement

--- src/functions.rs
+++ src/functions.rs
@@ -6,4 +6,12 @@ pub fn tell_height(height: u32) {
 
 pub fn my_id(name: &str, age: u32, height:f32) {
     println!("Hello, my name is {}, I am {} years old, and I am {} cm.", name, age, height);
+    
+    let x: i32 = {
+        let price: i32 = 10;
+        let quantity: i32 = 120;
+        price * quantity
+    };
+    println!("Total: {}", x);
+
 }
