8300c8301f849cca1d1044ffe8ce90317c167703
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-01-20 20:16:46 +0100

    constants

---
+++ src/bin/constants.rs
@@ -0,0 +1,9 @@
+fn main () {
+    const Y: i32 =  20;
+    println!("{}", Y);
+
+    println!("The value of PI is: {}", PI );
+}
+
+// NB: You can declare a constant with a type annotation
+const PI: f64 = 3.141592653;
