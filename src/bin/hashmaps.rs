350fc84537a62acd90ab700e1d1d64ac8ed3fe92
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-01-23 00:10:58 +0100

    updated

--- src/bin/hashmaps.rs
+++ src/bin/hashmaps.rs
@@ -5,9 +5,10 @@ fn main() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Purple"), 50);
 
     let team_name: String = String::from("Blue");
-    let score: i32 = scores.get(&team_name).copied().unwrap_or(0);
+    let _score: i32 = scores.get(&team_name).copied().unwrap_or(0);
 
     for (key, value) in &scores {
         println!("{}: {}", key, value);
