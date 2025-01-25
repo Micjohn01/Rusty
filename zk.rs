a8ab7cb4b409f552ab3b54cbda2bc0e19dc307dd
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-01-25 10:42:06 +0100

    other method for univariate

--- zk.rs
+++ zk.rs
@@ -8,18 +8,18 @@ impl UnivariatePoly {
     }
 
     fn evaluate(&self, x: f64) -> f64 {
+        let mut evaluation = 0.0;
+        for i in 0..self.coefficients.len() {
+           evaluation += self.coefficients[i] * x.powf(i as f64)
+        }
+        evaluation;
         // let mut evaluation = 0.0;
+        // let mut current_x = 1.0;
         // for i in 0..self.coefficients.len() {
-        //    evaluation += self.coefficients[i] * x.powf(i as f64)
+        //    evaluation += self.coefficients[i] * current_x;
+        //     current_x *= x;
         // }
         // evaluation
-        let mut evaluation = 0.0;
-        let mut current_x = 1.0;
-        for i in 0..self.coefficients.len() {
-           evaluation += self.coefficients[i] * current_x;
-            current_x *= x;
-        }
-        evaluation
 
         self.coefficients.iter().enumerate().map(|(i, coffe)| coffe * x.powf(i as f64)).sum()
     }
